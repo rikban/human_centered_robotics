5f52b6fc50696535a8e0390ad71a4cfb2a1e1034
Rik Banerjee <rik.banerjee1@gmail.com>
2022-02-18 17:00:51 -0700

    Trajectory following algorithm is ready

--- catkin_ws/src/project1/src/move_turtle.cpp
+++ catkin_ws/src/project1/src/move_turtle.cpp
@@ -46,6 +46,7 @@ float errorBetweenCurrentAndGoalPose(pose current_pose, pose goal_pose)
 {
     float pose_norm = 0;
     pose_norm = sqrt(pow((goal_pose.x - current_pose.x), 2) + pow((goal_pose.y - current_pose.y), 2));
+    std::cout<< "Norm of the pose error: "<<pose_norm<<std::endl;
     return pose_norm;
 }
 float calculateLinearVelocity(float pose_error)
@@ -56,7 +57,7 @@ float calculateLinearVelocity(float pose_error)
 
 float calculateAngularVelocity(float orientation_error)
 {
-    float proportional_angular_gain = 0.05;
+    float proportional_angular_gain = 0.5;
     return proportional_angular_gain*orientation_error;
 }
 int main(int argc, char **argv)
@@ -105,16 +106,25 @@ int main(int argc, char **argv)
         float orientation_error = M_PI/2;
         std::cout<< "Goal point: (" << goal[goal_point].x <<", "<< goal[goal_point].y<<")"<<std::endl;
 
-        while(pose_error > 0.1 && counter < 15)
+        while(pose_error > 0.01)
         {
-            float desired_heading = (M_PI + atan2(goal[goal_point].y, goal[goal_point].x));
-            while(abs(orientation_error) > 0.1 )
+            float desired_heading = atan2(( goal[goal_point].y - current_turtle_pose.y),
+                                         ( goal[goal_point].x - current_turtle_pose.x));
+           
+            ROS_INFO("Desired Heading: %.2f", desired_heading);
+            //(M_PI + atan2(goal[goal_point].y, goal[goal_point].x));
+          
+            while(abs(orientation_error) > 0.01 )
             {
 
-                orientation_error = current_turtle_pose.orientation - desired_heading;
+                float desired_heading = atan2(-(current_turtle_pose.y - goal[goal_point].y),
+                                         -(current_turtle_pose.x - goal[goal_point].x));
+           
+                ROS_INFO("Desired Heading: %.2f", desired_heading);
+                orientation_error = desired_heading - current_turtle_pose.orientation;
 
                 std::cout<<"orientation error: "<< orientation_error << std::endl;
-                 std::cout<< "Fred thinks he's at: "<< current_turtle_pose.x<< ", "
+                std::cout<< "Fred thinks he's at: "<< current_turtle_pose.x<< ", "
                      <<current_turtle_pose.y<<", "<< current_turtle_pose.orientation<<std::endl;
                 velocity_command.linear.x = 0;
                 velocity_command.angular.x = 0.0;
@@ -124,7 +134,7 @@ int main(int argc, char **argv)
                 ros::spinOnce();
                 rate.sleep();
             }
-            std::cout<< "I'm oriented and headed to the next trajectory point!!"<< std::endl;
+            //std::cout<< "I'm oriented and headed to the next trajectory point!!"<< std::endl;
             pose_error = errorBetweenCurrentAndGoalPose(current_turtle_pose, goal[goal_point]);
             
             //ROS_INFO("Pose error: %.2f", pose_error);
@@ -132,23 +142,36 @@ int main(int argc, char **argv)
            //          <<current_turtle_pose.y<<", "<< current_turtle_pose.orientation<<std::endl;
 
             //velocity_command.linear.x = calculateLinearVelocity(pose_error);
-            velocity_command.linear.x = 0;
+            velocity_command.linear.x = calculateLinearVelocity(pose_error);
             velocity_command.linear.y = 0.0;
             velocity_command.linear.z = 0.0;
             velocity_command.angular.x = 0.0;
             velocity_command.angular.y = 0.0;
-            velocity_command.angular.z = 0.0;
+            velocity_command.angular.z = calculateAngularVelocity(orientation_error);
 
 
             pub.publish(velocity_command);
-            std::cout<< "Requested: "<<velocity_command.linear.x << " forward and " 
-                                     << velocity_command.angular.z << " about z"<<std::endl;
-            ++counter;
+           // std::cout<< "Requested: "<<velocity_command.linear.x << " forward and " 
+            //                         << velocity_command.angular.z << " about z"<<std::endl;
+            std::cout<< "Fred thinks he's at: "<< current_turtle_pose.x<< ", "
+                     <<current_turtle_pose.y<<", "<< current_turtle_pose.orientation<<std::endl;
+            //++counter;
             ros::spinOnce();
             rate.sleep();
         }
         
+
+       // pose_error = errorBetweenCurrentAndGoalPose(current_turtle_pose, goal[goal_point]);
+        velocity_command.linear.x = 0.0;
+        pub.publish(velocity_command);
+        ros::spinOnce();
+        rate.sleep();
+        if (goal_point == 0)
+        {
+
+        }
         ++goal_point;
+        pose_error = errorBetweenCurrentAndGoalPose(current_turtle_pose, goal[goal_point]);
         ROS_INFO("Heading to the next trajectory point!");
 
     }
